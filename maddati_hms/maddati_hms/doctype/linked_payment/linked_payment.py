792258ce27277ab525a5d58c5b7429185d18dafb
Milan Thapa <tmilan0604@gmail.com>
2025-07-21 09:45:05 +0545

    code update

--- maddati_hms/maddati_hms/doctype/linked_payment/linked_payment.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, Maddati Tech and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class LinkedPayment(Document):
-	pass
